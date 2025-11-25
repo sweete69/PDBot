de30b61f275c73429d9f3b44f3f40735b3feb689
Tim <tksusha1973@gmail.com>
2025-11-25 21:25:25 +0300

    feat: added image/text description generation

--- Tgbot/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("78229333698:AAGcU3pYqoI-PNLTn4ZdUHLqu4kl4yJQ5fQ")
--- Tgbot/utils/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class CreateBanner(StatesGroup):
-    waiting_for_photo = State()
-    waiting_for_text = State()
-
-class CreateDescription(StatesGroup):
-    waiting_for_photo = State()
-    waiting_for_text = State()
